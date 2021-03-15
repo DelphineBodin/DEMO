1b0c580c107ad8fc0428f2a78b1aa4a78d4dd682
rroum <delphine.bodin44@gmail.com>
2021-03-15 14:18:29 +0100

    coucou

---
+++ script.php
@@ -0,0 +1,9 @@
+<?php
+$monTableau = [];
+$monTableau['maPremiereAssociation']="Julien";
+$monTableau['maDeuxiemeAssociation']="Bob";
+$var =$monTableau['maPremiereAssociation'];
+var_dump($monTableau);
+
+
+
